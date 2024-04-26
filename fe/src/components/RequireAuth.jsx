813281597823c0329c4d230e1dff2cd9359901cc
Mohammed Aslam <mohammedaslam4106@gmail.com>
2024-04-26 21:54:05 +0530

    removed the authentication

--- fe/src/components/RequireAuth.jsx
+++ fe/src/components/RequireAuth.jsx
@@ -2,7 +2,8 @@ import { Navigate } from "react-router-dom";
 import auth from "../auth";
 
 const RequireAuth = ({ children }) => {
-  return auth.isAuthenticated() ? children : <Navigate replace to="/login" />;
+  // return auth.isAuthenticated() ? children : <Navigate replace to="/login" />;
+  return children
 };
 
 export default RequireAuth;
